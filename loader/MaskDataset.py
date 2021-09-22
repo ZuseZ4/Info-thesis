b34e8998ef48949013803495acdb658c5fa0076a
Manuel Drehwald <git@manuel.drehwald.info>
2021-09-22 07:44:37 +0200

    update loader to work for generated, as well as OpenSurface Dataset

--- loader/MaskDataset.py
+++ loader/MaskDataset.py
@@ -19,16 +19,28 @@ import random
 # Now we have one equal and one different image
 class MaskDataset(Dataset):
     
-    def __init__(self, training_path):
-        self.img_dir = os.path.join(training_path, "img")
-        self.masks_dir = os.path.join(training_path, "masks")
+    def __init__(self, img_dir, mask_dir, image_ending=".png"):
+        self.img_dir = img_dir
+        self.masks_dir = mask_dir
         self.mask_subdirs = [f for f in os.scandir(self.masks_dir) if f.is_dir()]
+        self.image_ending = image_ending
         self.surface_names = [f.name for f in self.mask_subdirs]
         self.paths = [ [mask.path for mask in os.scandir(class_dir.path)] for class_dir in self.mask_subdirs]
         self.examples_per_surface = [len(path) for path in self.paths]
         self.num_classes = len(self.surface_names)
         self.len = sum(self.examples_per_surface)
+
+        # We might have classes without examples, strip them:
+        for i in range(self.num_classes-1, -1, -1):
+            if self.examples_per_surface[i] == 0:
+                self.paths.pop(i)
+                self.examples_per_surface.pop(i)
+                self.surface_names.pop(i)
+                self.mask_subdirs.pop(i)
+        self.num_classes = len(self.surface_names)
+
         print("Initialized", self.len, "objects from", self.num_classes, "classes.")
+        print(self.examples_per_surface)
 
     # Just for debug
     def get(self):
@@ -48,29 +60,32 @@ class MaskDataset(Dataset):
         # Now we have to pick two masks / images from our primary class, one from the secondary
         # We only pick our first primary img based on the index.
         # The other two masks / images are picked randomly from their selected class
-        primary_mask = self.paths[primary_class][index]
-        sym_mask = self.paths[primary_class][random.randrange(0, self.examples_per_surface[primary_class])]
-        diff_mask = self.paths[secondary_class][random.randrange(0, self.examples_per_surface[secondary_class])]
-        #print(primary_mask + "\n" + sym_mask + "\n" + diff_mask)
+        primary_mask_path = self.paths[primary_class][index]
+        sym_mask_path     = self.paths[primary_class][random.randrange(0, self.examples_per_surface[primary_class])]
+        diff_mask_path    = self.paths[secondary_class][random.randrange(0, self.examples_per_surface[secondary_class])]
+        print(primary_mask_path + "\n" + sym_mask_path + "\n" + diff_mask_path)
         
         # Now we have to get the corresponding input images for these masks
-        primary_num = primary_mask.split(os.path.sep)[-1].split("_")[0] + ".png"
-        sym_num = sym_mask.split(os.path.sep)[-1].split("_")[0] + ".png"
-        diff_num = diff_mask.split(os.path.sep)[-1].split("_")[0] + ".png"
-        primary_img = os.path.join(self.img_dir,primary_num)
-        sym_img = os.path.join(self.img_dir,sym_num)
-        diff_img = os.path.join(self.img_dir,diff_num)
-        #print(primary_img + "\n" + sym_img + "\n" + diff_img)  
+        # We remove the image ening and add the ending which our masks will have
+        # We remove _X enumerations which show up if we have multiple masks based of the same class for one image
+        primary_name = os.path.basename(primary_mask_path).split(".")[0].split("_")[0] + self.image_ending
+        sym_name     = os.path.basename(sym_mask_path).split(".")[0].split("_")[0] + self.image_ending
+        diff_name    = os.path.basename(diff_mask_path).split(".")[0].split("_")[0] + self.image_ending
+
+        primary_img_path = os.path.join(self.img_dir, primary_name)
+        sym_img_path     = os.path.join(self.img_dir, sym_name    )
+        diff_img_path    = os.path.join(self.img_dir, diff_name   )
+
         
         # Now we just have to replace all the paths by the actual images / masks 
-        primary_mask = Image.open(primary_mask)
-        sym_mask = Image.open(sym_mask)
-        diff_mask = Image.open(diff_mask)
+        primary_mask = Image.open(primary_mask_path)
+        sym_mask = Image.open(sym_mask_path)
+        diff_mask = Image.open(diff_mask_path)
         
-        # TODO: Images should be generated as RGB's by blender, not RGBA. Fix it there
-        primary_img = Image.open(primary_img).convert("RGB")
-        sym_img = Image.open(sym_img).convert("RGB")
-        diff_img = Image.open(diff_img).convert("RGB")
+        # I told Blender to output RGB instead of RGBA, so that's just making sure.
+        primary_img = Image.open(primary_img_path).convert("RGB")
+        sym_img = Image.open(sym_img_path).convert("RGB")
+        diff_img = Image.open(diff_img_path).convert("RGB")
         #print("mask mode: ", primary_mask.mode, " image mode: ", primary_img.mode) # 1, RGB
 
         
